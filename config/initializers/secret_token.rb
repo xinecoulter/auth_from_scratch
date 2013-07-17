4b5734c78a59026f72c18c22a303131d32358c68
David Fisher <tibbon+github@gmail.com>
2013-07-16 23:24:34 -0400

    Created controller and model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthFromScratch::Application.config.secret_token = 'b8436619a41bbeecddcabb82159a91015b0cf54cfe32b68647d9567e073fc016aa5f1621d5dbafa6d9f7eb92a751bdabc8523c70e358f50e0e0effe8ca0b2b62'
