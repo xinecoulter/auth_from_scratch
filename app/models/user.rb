51b2351f55a2c1bd751f3049531c1b880ecae8f0
David Fisher <tibbon+github@gmail.com>
2013-07-16 23:30:38 -0400

    Added bcrypt to Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_hash, :password_salt
+  attr_accessor :password
+  validates_confirmation_of :password
+  validates_presence_of :password, on: :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
