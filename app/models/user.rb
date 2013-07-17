2da27bfdc0ea79fb475796618d879ecf20944ecf
David Fisher <tibbon+github@gmail.com>
2013-07-17 10:18:14 -0400

    Class time

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, password)
 	  user = find_by_email(email)
-	  if user && user.password_hash == BCrypt::Engine.hash_secret (password, user.password_salt)
+	  if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
 	    user
 	  else
 	    nil
